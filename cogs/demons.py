566fa1a0bc03f45552d23425eae16f32e0060cfb
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2025-05-08 19:17:26 +0700

    Update demons.py

--- cogs/demons.py
+++ cogs/demons.py
@@ -372,7 +372,7 @@ và cũng mong đối phương sẽ ko đả động hay gây ảnh hưởng gì
                 (id, thread_name, _, _, _, _) = row
 
         thread = await nsfw_channel.create_thread(
-            name=f"[Hội Nghĩa] {thread_name}",
+            name=f"[Hội Thanh Hóa] {thread_name}",
             type=discord.ChannelType.private_thread,
             invitable=True,
         )
